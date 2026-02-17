2805ad7d6aa82f8be7b49b0343e1ac7ebb26caba
Eric Scott <scottericr@gmail.com>
2026-02-17 10:58:57 -0700

    arrange pop_info by date just in case

--- R/fia_assign_strata.R
+++ R/fia_assign_strata.R
@@ -44,6 +44,7 @@ fia_assign_strata <- function(data_annualized, db) {
 
   chosen_evals <- pop_info |>
     dplyr::filter(EXPVOL & EXPCURR) |>
+    dplyr::arrange(INVYR, START_INVYR, END_INVYR) |>
     dplyr::select(-INVYR)
 
   # Rolling join to match all EVALIDs containing YEAR
